ef3f04fe08b8a7f1805b1881c5493307a8368e48
kun <yykunkun@126.com>
2017-01-15 01:19:45 +0800

    2017.1.14 01:20am等服务器

--- ForSix/app/src/main/java/com/project_test/forsix/ShowCloudFilesActivity.java
+++ ForSix/app/src/main/java/com/project_test/forsix/ShowCloudFilesActivity.java
@@ -4,19 +4,24 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class ShowCloudFilesActivity extends AppCompatActivity {
-
+    private ListView cloudFiles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_cloud_files);
         init();
+
+        //TODO:retrofit请求框架，请求列表，显示；
     }
 
     private void init() {
         confirmConnectify();
+        cloudFiles= (ListView) findViewById(R.id.cloud_files);
+
     }
 
     private void confirmConnectify() {
---
+++ ForSix/app/src/main/java/com/project_test/forsix/URLs.java
@@ -0,0 +1,13 @@
+package com.project_test.forsix;
+
+/**
+ * Created by kun on 2017/1/15.
+ */
+
+public class URLs {
+    public static final String fileListURL="http://pan.dadoo.im:7676/list";//查询文件列表
+    public static final String downloadURL=" http://pan.dadoo.im:7676/download";//下载
+    public static final String uploadURL=" http://pan.dadoo.im:7676/upload";//上传
+    public static final String createDirURL="http://pan.dadoo.im:7676/mkdir";//创建文件夹
+
+}
