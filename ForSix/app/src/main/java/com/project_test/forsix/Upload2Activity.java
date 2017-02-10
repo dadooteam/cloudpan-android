f0809b6b46a112409c4dfdf42de1a9c7b9c87587
yykunkun <1390123333>
2017-02-10 16:31:41 +0800

    V0.0.0.2定稿

--- ForSix/app/src/main/java/com/project_test/forsix/Upload2Activity.java
+++ ForSix/app/src/main/java/com/project_test/forsix/Upload2Activity.java
@@ -6,12 +6,14 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,13 +48,21 @@ public class Upload2Activity extends AppCompatActivity {
     private Stack<String> nowPathStack;
     private String currentPath;
     private Retrofit retrofit;
-
+    private RelativeLayout frame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload2);
         instance = this;
+        frame= (RelativeLayout) findViewById(R.id.centeral_local_file);
+        Snackbar.make(frame, "右滑显示上传列表", Snackbar.LENGTH_SHORT)
+                .setAction("知道了", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                    }
+                })
+                .show();
         retrofit = new Retrofit.Builder()
                 .baseUrl(URLs.BASEURL)
                 .addConverterFactory(GsonConverterFactory.create())
